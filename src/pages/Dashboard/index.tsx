e3dae1235d3e4de3301f0b5c7a86079db0c27725
Mhayk Whandson <hi@mhayk.com>
2020-04-25 18:25:33 +0100

    The dashboard's style has been finished.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +15,33 @@ const Dashboard: React.FC = () => {
         <input placeholder="Type the repository's name" />
         <button type="submit">Search</button>
       </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars2.githubusercontent.com/u/1500873?s=460&u=5a9ec92a1b593df93e818429effaf392df609119&v=4"
+            alt="Mhayk Whandson"
+          />
+          <div>
+            <strong>mhayk/test</strong>
+            <p>Description about the repository!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars2.githubusercontent.com/u/1500873?s=460&u=5a9ec92a1b593df93e818429effaf392df609119&v=4"
+            alt="Mhayk Whandson"
+          />
+          <div>
+            <strong>mhayk/test</strong>
+            <p>Description about the repository!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
