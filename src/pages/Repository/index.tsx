ed1f835fc6077449a89006c9a962162b302abaf0
Mhayk Whandson <hi@mhayk.com>
2020-04-27 21:40:24 +0100

    The repository front end has been finished.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -4,7 +4,7 @@ import { FiChevronLeft } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Header } from './styles';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -14,13 +14,53 @@ const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
   return (
-    <Header>
-      <img src={logoImg} alt="Github Explorer" />
-      <Link to="/dashboard">
-        <FiChevronLeft size={16} />
-        Voltar
-      </Link>
-    </Header>
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars2.githubusercontent.com/u/1500873?s=460&u=5a9ec92a1b593df93e818429effaf392df609119&v=4"
+            alt="Mhayk Whandson"
+          />
+          <div>
+            <strong>mhayk/github-explorer</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="fdwffwef">
+          <div>
+            <strong>Repository.full_name</strong>
+            <p>Repository.description</p>
+          </div>
+
+          <FiChevronLeft size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
